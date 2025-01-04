d91bac57625ad22eefc7cde2124a67ce46a8239f
BH <bcosta@alunos.utfpr.edu.br>
2025-01-04 00:04:27 -0300

    Revert "Update Navbar.vue"
    This reverts commit 8378b0b84d074f1313c9436e3355c5f98f87b5ce.

--- public/src/components/Navbar.vue
+++ public/src/components/Navbar.vue
@@ -2,9 +2,9 @@
   <div class="navbar">
     <!-- Logotipo ou título à esquerda -->
     <div class="navbar-left">
-      <a href="/">
-  <img :src="require('~/assets/images/logo.png')" alt="Logo" class="navbar-logo" />
-</a>
+        <a href="/">
+          <img :src="require('~/assets/images/logo.png')" alt="Logo" class="navbar-logo" />
+        </a>
     </div>
 
     <!-- Links de navegação -->
