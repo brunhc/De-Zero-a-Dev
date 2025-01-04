bddb03cba037da9db333fe16269dd92002964b2a
BH <bcosta@alunos.utfpr.edu.br>
2025-01-04 01:54:18 -0300

    Corrigindo configuração para Nuxt 3 e GitHub Pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,13 @@
+import { defineNuxtConfig } from 'nuxt/config';
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  target: 'static',
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   css: [
     '~/assets/css/global.css', // Importa o CSS global
   ],
-  router: {
-    base: '/De-Zero-a-Dev/', // Substitua pelo nome do seu repositório no GitHub
-  }
-})
+  app: {
+    baseURL: '/De-Zero-a-Dev/', // Substitua pelo nome do seu repositório no GitHub
+  },
+});
